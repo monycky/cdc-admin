1f6fe006244f3cb4de083e97568567a534ee86de
monycky <monyckyfrazao@gmail.com>
2017-06-29 17:45:17 -0300

    Import css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './css/pure-min.css';
+import './css/side-menu.css';
 
 class App extends Component {
   render() {
