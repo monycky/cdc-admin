473f8554d4654d7ca25481bf65db660514155197
Leandro Araujo <leandro.nossoceara@gmail.com>
2017-07-03 13:30:15 -0300

    fix: Change 'sucess' to 'success'.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './css/pure-min.css';
-import './css/side-menu.css';
-import $ from 'jquery';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./css/pure-min.css";
+import "./css/side-menu.css";
+import $ from "jquery";
 
 class App extends Component {
-
   constructor() {
     super();
-    this.state = { lista: [], nome: '', email: '', senha: '' };
+    this.state = { lista: [], nome: "", email: "", senha: "" };
     this.enviaForm = this.enviaForm.bind(this);
     this.setNome = this.setNome.bind(this);
     this.setEmail = this.setEmail.bind(this);
     this.setSenha = this.setSenha.bind(this);
-
-
   }
 
   componentDidMount() {
     $.ajax({
-      url: 'http://cdc-react.herokuapp.com/api/autores',
-      dataType: 'json',
-      sucess: function (resposta) {
+      url: "http://cdc-react.herokuapp.com/api/autores",
+      dataType: "json",
+      success: function(resposta) {
         this.setState({ lista: resposta });
       }.bind(this)
-    }
-    );
+    });
   }
 
   enviaForm(evento) {
     evento.preventDefault();
 
     $.ajax({
-      url: 'http://cdc-react.herokuapp.com/api/autores',
+      url: "http://cdc-react.herokuapp.com/api/autores",
       contentType: "application/json",
-      dataType: 'json',
-      type: 'post',
-      data: JSON.stringify({ nome: this.state.nome, email: this.state.email, senha: this.state.senha }),
-      sucess: function (resposta) {
+      dataType: "json",
+      type: "post",
+      data: JSON.stringify({
+        nome: this.state.nome,
+        email: this.state.email,
+        senha: this.state.senha
+      }),
+      success: function(resposta) {
         console.log("deu bom");
         this.setState({ lista: resposta });
       }.bind(this),
-      error: function (resposta) {
+      error: function(resposta) {
         console.log("deu ruim");
         console.log(resposta);
       }
@@ -56,26 +56,30 @@ class App extends Component {
     this.setState({ email: evento.target.value });
   }
 
-
   setSenha(evento) {
     this.setState({ senha: evento.target.value });
   }
-
 
   render() {
     return (
       <div id="layout">
         <a href="#menu" id="menuLink" className="menu-link">
-          <span></span>
+          <span />
         </a>
         <div id="menu">
           <div className="pure-menu">
             <a className="pure-menu-heading" href="#">Company</a>
 
             <ul className="pure-menu-list">
-              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-              < li className="pure-menu-item"><a href="#" className="pure-menu-link">Autores</a></li>
-              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livros</a></li>
+              <li className="pure-menu-item">
+                <a href="#" className="pure-menu-link">Home</a>
+              </li>
+              <li className="pure-menu-item">
+                <a href="#" className="pure-menu-link">Autores</a>
+              </li>
+              <li className="pure-menu-item">
+                <a href="#" className="pure-menu-link">Livros</a>
+              </li>
 
             </ul>
           </div>
@@ -87,22 +91,49 @@ class App extends Component {
           </div>
           <div className="content" id="content">
             <div className="pure-form pure-form-aligned">
-              <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+              <form
+                className="pure-form pure-form-aligned"
+                onSubmit={this.enviaForm}
+                method="post"
+              >
                 <div className="pure-control-group">
                   <label htmlFor="nome">Nome</label>
-                  <input id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} />
+                  <input
+                    id="nome"
+                    type="text"
+                    name="nome"
+                    value={this.state.nome}
+                    onChange={this.setNome}
+                  />
                 </div>
                 <div className="pure-control-group">
                   <label htmlFor="email">Email</label>
-                  <input id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} />
+                  <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.setEmail}
+                  />
                 </div>
                 <div className="pure-control-group">
                   <label htmlFor="senha">Senha</label>
-                  <input id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} />
+                  <input
+                    id="senha"
+                    type="password"
+                    name="senha"
+                    value={this.state.senha}
+                    onChange={this.setSenha}
+                  />
                 </div>
                 <div className="pure-control-group">
-                  <label></label>
-                  <button type="submit" className="pure-button pure-button-primary">Gravar</button>
+                  <label />
+                  <button
+                    type="submit"
+                    className="pure-button pure-button-primary"
+                  >
+                    Gravar
+                  </button>
                 </div>
               </form>
 
@@ -116,17 +147,14 @@ class App extends Component {
                   </tr>
                 </thead>
                 <tbody>
-                  {
-                    this.state.lista.map(function (autor) {
-                      return (
-                        <tr>
-                          <td>{autor.nome}</td>
-                          <td>{autor.email}</td>
-                        </tr>
-                      );
-
-                    })
-                  }
+                  {this.state.lista.map(function(autor) {
+                    return (
+                      <tr>
+                        <td>{autor.nome}</td>
+                        <td>{autor.email}</td>
+                      </tr>
+                    );
+                  })}
                 </tbody>
               </table>
             </div>
