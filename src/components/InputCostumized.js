5371b5f7551670963003bade228ddadd87ffd5f1
monycky <monyckyfrazao@gmail.com>
2017-07-18 22:56:37 -0300

    Changing components for error handling

--- src/components/InputCostumized.js
+++ src/components/InputCostumized.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
 export default class InputCostumized extends Component {
+    
+    constructor(){
+        super();
+        this.state = {msgErro: ''};
+    }
 
     render() {
         return (
             <div className="pure-control-group">
                 <label htmlFor={this.props.id}>{this.props.label}</label>
                 <input id={this.props.id} type={this.props.type} name={this.props.name} value={this.props.value} onChange={this.props.onChange}/>
+                <span className="error">{this.state.msgErro} </span>
             </div>
         );
     }
