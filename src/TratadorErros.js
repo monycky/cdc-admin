53d95fc5543d1a08f7545ab60d82931f38bbb1bc
monycky <monyckyfrazao@gmail.com>
2017-07-20 14:10:11 -0300

    fix: conflitct between variables

--- src/TratadorErros.js
+++ src/TratadorErros.js
@@ -2,15 +2,9 @@ import PubSub from 'pubsub-js';
 
 export default class TratadorErros{
   publicaErros(erros){
-      for(var i=0; i<erros.error.length; i++){
-          var erro = erros.erros[i];
+      for(var i=0; i<erros.errors.length; i++){
+          var erro = erros.errors[i];
           PubSub.publish("erro-validacao", erro);   
       }
-  }
-
-  componentDidMount(){
-       PubSub.subscribe('erro-validacao', function(topico, erro){
-        this.setState({msgErro: erro.error})
-       }.bind(this));
   }  
 } 
